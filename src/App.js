d3c3f2f211a0bae911c0c43a1ad3deace81e92dd
divythakur <divyanshuthakur75@gmail.com>
2020-08-20 16:06:50 +0530

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom'
+const request=require('request');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      det:[]
+    }
+  }
+  componentDidMount(){
+    const url="https://backendpart.herokuapp.com/user";
+    request({url:url,json:true},(error,response)=>{
+      const b=response.body;
+      this.setState({det:b});
+
+    })
+   }
+
+  render(){
+    return(
+      <div style={{background:"aquamarine"}}>
+        <h1 style={{textAlign:"center",fontSize:"45px",color:"darkgoldenrod"}}>Welcome to Movies City</h1>
+        {
+          this.state.det.map((ele)=>{
+            return(
+              <div key={ele._id}>
+              <p style={{fontSize:"37px",color:"brown",fontWeight:"900",textDecoration:"underline"}}>{ele.name}</p>
+              <img src={ele.img} height="300px" width="300px" alt="URL given in the database is dead"></img>
+            <p style={{color:"darkmagenta",fontSize:"25px"}}>{ele.summary}</p>
+              </div>
+            )
+          })
+        }
+
+      </div>
+    )
+  }
 }
 
 export default App;

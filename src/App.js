8e30ca89495928b07737ec0049816b21de5bcb0f
divythakur <divyanshuthakur75@gmail.com>
2020-09-22 20:32:14 +0530

    slight changes in model Schema

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios'
+
 import {BrowserRouter} from 'react-router-dom'
 const request=require('request');
 
@@ -7,34 +9,109 @@ class App extends React.Component{
   constructor(){
     super();
     this.state={
-      det:[]
+      name:"",
+      email:"",
+      password:"",
+      loginemail:"",
+      loginpassword:""
     }
   }
   componentDidMount(){
-    const urll="https://backendpart.herokuapp.com/user";
-    request({url:urll,json:true},(error,response)=>{
-      const b=response.body;
-      this.setState({det:b});
+  //   const urll="https://backendpart.herokuapp.com/user";
+  //   request({url:urll,json:true},(error,response)=>{
+  //     const b=response.body;
+  //     this.setState({det:b});
 
+  //   })
+    }
+    onchangename=(e)=>{
+     
+    
+
+    }
+    onchangeemail=(e)=>{
+      this.setState({email:e.target.value})
+
+    }
+    onchangeloginemail=(e)=>{
+      this.setState({loginemail:e.target.value})
+
+    }
+    onchangeloginpassword=(e)=>{
+      this.setState({loginpassword:e.target.value})
+    }
+    onchangepassword=(e)=>{
+      this.setState({password:e.target.value})
+    }
+
+    buttonclicked=(e)=>{
+      e.preventDefault();
+      var body = {
+        name:this.state.name,
+        email: this.state.email,
+        password:this.state.password
+    }
+    console.log(body.name)
+    
+    axios({
+        method: 'post',
+        url: 'http://localhost:8000/user/add',
+        data: body
     })
-   }
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+    }
+    
+    loginclicked=(e)=>{
+      e.preventDefault();
+      var url="http://localhost:8000/user/getone/"
+      var em=this.state.loginemail;
+       url=url+em
+      axios.get(url)
+      .then((response)=>{console.log(response.data)
+        if(response.data==={})
+        {
+          alert("not registered")
+        }
+        else if(response.data===this.state.loginpassword)
+        {
+          alert("success")
+        }
+        else if( response.data!=={} && response.data!==this.state.loginpassword)
+        {
+          alert("not success")
+        }
+      })
+
+    }
 
   render(){
     return(
-      <div style={{background:"aquamarine"}}>
-        <h1 style={{textAlign:"center",fontSize:"45px",color:"darkgoldenrod"}}>Welcome to Movies City</h1>
-        {
-          this.state.det.map((ele)=>{
-            return(
-              <div key={ele._id}>
-              <p style={{fontSize:"37px",color:"brown",fontWeight:"900",textDecoration:"underline"}}>{ele.name}</p>
-              <img src={ele.img} height="300px" width="300px" alt="URL given in the database is dead"></img>
-            <p style={{color:"darkmagenta",fontSize:"25px"}}>{ele.summary}</p>
-              </div>
-            )
-          })
-        }
+      <div>
+        
+          <input type='text' name="email" placeholder="enter email"  onChange={this.onchangeemail}/>
+          <input type="password" name="password" placeholder="enter password" onChange={this.onchangepassword} />
+          <input type="text" name="name" placeholder="enter name" onChange={this.onchangename} />
+          <button type="submit" onClick={this.buttonclicked}>Click </button>
+          
+          <br/><br/><br/>
+          <input type="text" name="loginemail" onChange={this.onchangeloginemail} />
+          <input type="password" name="loginpassword" onChange={this.onchangeloginpassword} />
+          <button type="submit" onClick={this.loginclicked}>login </button>
+          <p>here{this.state.loginemail}</p>
 
+
+
+
+
+          
+
+      
       </div>
     )
   }
